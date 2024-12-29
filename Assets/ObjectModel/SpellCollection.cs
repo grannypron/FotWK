8167a8a72a9e9c63ccb51371dc2ac78190093281
grannypron <andrew_steeley@hotmail.com>
2024-12-29 10:43:32 -0500

    Refactor

---
+++ Assets/ObjectModel/SpellCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FotWK
+{
+    public class SpellCollection : Dictionary<SpellType, int>
+    {
+        public SpellCollection()
+        {
+            // Initialize all to 0 - be careful with this if new spells are added later and old save states are restored
+            foreach (SpellType spellType in Enum.GetValues(typeof(SpellType))) {
+                this.Add(spellType, 0);
+            }
+        }
+    }
+}
---
+++ Assets/ObjectModel/SupportUnitCollection.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FotWK
+{
+	public enum SupportUnitType
+    {
+		Mule = 1
+    }
+
+	public class SupportUnitCollection : Dictionary<SupportUnitType, int>
+	{
+        public SupportUnitCollection()
+        {
+            // Initialize all to 0 - be careful with this if new spells are added later and old save states are restored
+            foreach (SupportUnitType supportUnitType in Enum.GetValues(typeof(SupportUnitType))) {
+                this.Add(supportUnitType, 0);
+            }
+        }
+    }
+}
